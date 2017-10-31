ce2b56918ba0296a7c2a7ea6832b9973428e0c42
ryunosinfx <ryunosinfx@livedoor.com>
2017-10-31 15:14:51 +0900

    app ファイル追加3

--- src/util/service.js
+++ src/util/service.js
@@ -1,1 +1,13 @@
 import Authoricator from 'encrypt-indexeddb-entity-manager/src/core/auth/authoricator'
+export default class Servicess {
+  constructor() {
+    this.authoricator = new Authoricator();
+    this.DBSyncronizer = new DBSyncronizer(this.authoricator);
+  }
+  show(){
+
+  }
+  async activste(){
+
+  }
+}
