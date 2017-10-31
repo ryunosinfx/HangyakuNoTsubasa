85e13920251d601fe9deac346d6f517e0ff36438
ryunosinfx <ryunosinfx@livedoor>
2017-11-01 00:51:54 +0900

    しかかり3

--- src/util/service.js
+++ src/util/service.js
@@ -1,13 +1,26 @@
 import Authoricator from 'encrypt-indexeddb-entity-manager/src/core/auth/authoricator'
+import DBScanner from 'encrypt-indexeddb-entity-manager/src/core/dbScanner'
+
 export default class Servicess {
   constructor() {
     this.authoricator = new Authoricator();
-    this.DBSyncronizer = new DBSyncronizer(this.authoricator);
+    this.DBSyncronizer = new DBScanner(this.authoricator);
   }
-  show(){
-
+  async registerUser(userId,password)){
+    await this.authoricator.signup(userId,password);
+  }
+  async login(userId,password)){
+    await this.authoricator.signin(userId,password);
   }
   async activste(){
-
+    await this.authoricator.activate(userId,password);
+  }
+  /*
+  */
+  async isLogiedIn(){
+    return await this.authoricator.isLogedIn();
+  }
+  async getQuery(){
+    
   }
 }
