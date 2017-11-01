d22d55a5fe23f5595583be13dffdd0b1609803ce
ryunosinfx <ryunosinfx@livedoor.com>
2017-11-01 16:03:39 +0900

    app ファイル追加4

--- src/util/service.js
+++ src/util/service.js
@@ -1,26 +1,31 @@
 import Authoricator from 'encrypt-indexeddb-entity-manager/src/core/auth/authoricator'
 import DBScanner from 'encrypt-indexeddb-entity-manager/src/core/dbScanner'
+import Query from 'encrypt-indexeddb-entity-manager/src/entity/query/query'
 
 export default class Servicess {
   constructor() {
     this.authoricator = new Authoricator();
-    this.DBSyncronizer = new DBScanner(this.authoricator);
+    this.dbScanner = new DBScanner(this.authoricator);
   }
-  async registerUser(userId,password)){
-    await this.authoricator.signup(userId,password);
+  async registerUser(userId, password) {
+
+    await this.authoricator.signup(userId, password);
   }
-  async login(userId,password)){
-    await this.authoricator.signin(userId,password);
+  async login(userId, password) {
+    await this.authoricator.signin(userId, password);
   }
-  async activste(){
-    await this.authoricator.activate(userId,password);
+  async activste() {
+    await this.authoricator.activate(userId, password);
   }
   /*
-  */
-  async isLogiedIn(){
+   */
+  async isLogiedIn() {
     return await this.authoricator.isLogedIn();
   }
-  async getQuery(){
-    
+  async createEntityManager() {
+    if (await this.isLogiedIn()) {
+      return new Query(isOnTranzaction, this.dbScanner);
+    }
+    return null;
   }
 }
