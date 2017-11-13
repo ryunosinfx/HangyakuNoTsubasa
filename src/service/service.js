001b4978e7067806ca9204178aaf580759046da9
ryunosinfx <ryunosinfx@livedoor.com>
2017-11-13 15:14:45 +0900

    app ファイル追加10

--- src/service/service.js
+++ src/service/service.js
@@ -12,12 +12,12 @@ import State from './state'
 const router = new Router();
 export default class Servicess {
   constructor() {
-    router.add(new Login());
-    router.add(new Activate());
-    router.add(new Viewer());
-    router.add(new Manage());
-    router.add(new Editor());
-    router.add(new Viewer());
+    router.add(new Login(),(state)=>{return state.isLogiedIn===false});
+    router.add(new Activate(),(state)=>{return state.isActivated===false});
+    router.add(new Viewer(),(state)=>{return state.isLogiedIn});
+    router.add(new Manage(),(state)=>{return state.isLogiedIn});
+    router.add(new Editor(),(state)=>{return state.isLogiedIn});
+    router.add(new Viewer(),(state)=>{return state.isLogiedIn});
   }
   async registerUser(userId, password) {
     await ECIDBEMfunc.signup(userId, password);
@@ -30,6 +30,12 @@ export default class Servicess {
   }
   /*
    */
+   async getCurrentState(){
+     let rettState = new State();
+     rettState.isLogedIn = await ECIDBEMfunc.isLogedIn();
+     rettState.isActivated = await ECIDBEMfunc.isActivated();
+     return rettState;
+   }
   async isLogiedIn() {
     return await ECIDBEMfunc.isLogedIn();
   }
@@ -41,8 +47,9 @@ export default class Servicess {
   }
 
   async goToNext（ key） {
-    let state = await this.loadState();
-    let page = router.getPage(state);
+    let state = await this.getCurrentState();
+    let page = router.getPage(state,key）;
+    page.show();
     return;
   }
   async loadState(){
