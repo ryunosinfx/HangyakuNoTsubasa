0b97582879674be2a61ec38146e9c6df8385deea
ryunosinfx <ryunosinfx@livedoor.com>
2017-11-14 14:54:47 +0900

    app ファイル追加11

--- src/view/parts/layout.js
+++ src/view/parts/layout.js
@@ -1,12 +1,24 @@
-import {patch, h} from 'encrypt-indexeddb-entity-manager/src/view/preLoader'
+import {
+  patch,
+  h
+} from 'encrypt-indexeddb-entity-manager/src/view/preLoader'
 import BaseView from '../baseView'
+import Header from './header'
+import Footer from './footer'
+import Menue from './menu'
 export default class Layout extends BaseView {
   constructor() {
     super.constructor();
-      super.constructor();
+    super.header = new Header();
+    super.footer = new Footer();
+    super.menu = new Menue();
+    this.baseFrame=null;
   }
-  show(page){
+  show(page) {
 
   }
-  add(view){}
+  add(view) {}
+  createBsaeFrame(){
+
+  }
 }
